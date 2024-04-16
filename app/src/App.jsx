a5a0a0111d4749054673ce86a1d8300f064b7fd3
Shahvinit3113 <shahvinit3152003@gmail.com>
2024-04-16 13:22:40 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,23 @@
+import Card from "./Card"
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+     <Card 
+     userName ="rutu"
+     btnText='Touch me'/>
+
+     <Card 
+     userName ="shree" 
+     btnText='Click me'/>
+     <Card />
+     
+     </div>
+    </>
+  )
+}
+
+export default App
